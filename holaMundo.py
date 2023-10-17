5fd6d8e1418658e2d79635f9eed43dd95545eea4
mfradesIA <148188249+mfradesIA@users.noreply.github.com>
2023-10-17 15:06:18 +0200

    Hola mundo - Tarea 01
    Hola mundo - Tarea 01

---
+++ holaMundo.py
@@ -0,0 +1,7 @@
+# Programa de saludo - Hola mundo
+# Módulo profesional - Programación en Inteligencia Artificial - Tarea 1
+# Desarrollado por María Frades
+# Fecha: 17/10/2023
+
+# Imprimimos el saludo en la consola
+print("Hola, mundo!")
